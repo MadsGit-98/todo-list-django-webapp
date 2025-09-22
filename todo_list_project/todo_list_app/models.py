91e64009cb5fe187da6701aaf22a407e1d47c494
MadsGit-98 <amr.hammad.dev25@gmail.com>
2025-09-22 18:10:16 +0300

    feat: Add To-Do List and List Item models with initial migration
    This commit introduces the To-Do List and List Item models to the application. Key changes include:
    
    - Creation of the `ToDoList` model with a foreign key to the user and a name attribute.
    - Creation of the `ListItem` model with a foreign key to the To-Do list, a text description, and a completion status.
    - Addition of an initial migration file to set up the database schema for these models.

--- todo_list_project/todo_list_app/models.py
+++ todo_list_project/todo_list_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# To-Do List Model
+class ToDoList(models.Model): 
+    """ Represents a To-Do list with name, and user
+    
+    Attributes: 
+        user(ForeignKey): The user that owns the To-Do List.  [1:* Relationship, a user can have multiple To-Do Lists]
+        name(CharField): The name of the To-Do List that the user has specified, with a maximum length of 64 characters.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+# List Items Class
+class ListItem(models.Model):
+    """ Represents a List Item with a containing list, text and status
+    
+    Attributes: 
+        list(ForeignKey): The ToDo list that this item lies in.  [1:* Relationship, a ToDo List can have multiple ListItems]
+        text(CharField): Description of a task or a To-Do list item, with a maximum length of 100 characters.
+        isCompleted(BooleanField): Whether the task is completed or not.
+    """
+    list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    isCompleted = models.BooleanField(default=False)
+
